271f318da383c5ac09e7a952f94ae962da8a74b7
Sergio Aroca Fernandez <saroca-f@c3r4s6.42malaga.com>
2024-05-16 15:56:57 +0200

    litle correction

--- tokenizer/tokenizer.h
+++ tokenizer/tokenizer.h
@@ -6,39 +6,38 @@
 /*   By: saroca-f <saroca-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 18:32:39 by saroca-f          #+#    #+#             */
-/*   Updated: 2024/05/15 18:35:25 by saroca-f         ###   ########.fr       */
+/*   Updated: 2024/05/16 15:56:38 by saroca-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <fcntl.h>
-# include <stdarg.h>
-# include <ctype.h>
-# include <sys/wait.h>
-# include <errno.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-# include <signal.h>
-# include <termios.h>
-# include <curses.h>
-# include <dirent.h>
-# include "./libft/include/libft.h" 
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <ctype.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <signal.h>
+#include <termios.h>
+#include <curses.h>
+#include <dirent.h>
 
-# define RED		"\x1b[31m"
-# define GREEN   	"\x1b[32m"
-# define YELLOW  	"\x1b[33m"
-# define BLUE    	"\x1b[34m"
-# define MAGENTA	"\x1b[35m"
-# define CYAN    	"\x1b[36m"
-# define WHITE		"\033[37m"
-# define GREYM		"\x1B[38;5;245m"
-# define GREYM2		"\x1B[38;5;250m"
-# define BOLD		"\033[1m"
-# define RESET   	"\x1b[0m"
-# define CLEAR		"\033[2J"
+#define RED			"\x1b[31m"
+#define GREEN   	"\x1b[32m"
+#define YELLOW  	"\x1b[33m"
+#define BLUE    	"\x1b[34m"
+#define MAGENTA		"\x1b[35m"
+#define CYAN    	"\x1b[36m"
+#define WHITE		"\033[37m"
+#define GREYM		"\x1B[38;5;245m"
+#define GREYM2		"\x1B[38;5;250m"
+#define BOLD		"\033[1m"
+#define RESET   	"\x1b[0m"
+#define CLEAR		"\033[2J"
 
 typedef enum e_tokentype
 {
