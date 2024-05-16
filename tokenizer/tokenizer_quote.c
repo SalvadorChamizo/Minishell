662637b290777a2ec6713f216d61801e63e34ef4
Sergio Aroca Fernandez <saroca-f@c3r4s6.42malaga.com>
2024-05-16 11:45:40 +0200

    Lexer directory:
    	Identiffer: Simple words and quotes sentences.
    	Redirecttions: Less, double less, great and double great.
    	Pipe.
    	Assigments.

---
+++ tokenizer/tokenizer_quote.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenizer_quote.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saroca-f <saroca-f@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/09 18:46:42 by saroca-f          #+#    #+#             */
+/*   Updated: 2024/05/16 11:29:33 by saroca-f         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "tokenizer.h"
+
+void	s_quote_case(t_input *mshll, t_token *nt, char *rt, t_tokentype t)
+{
+	int	start;
+
+	start = mshll->pos;
+	mshll->pos++;
+	while (mshll->line[mshll->pos] != '\'' && mshll->line[mshll->pos])
+		mshll->pos++;
+	if (mshll->line[mshll->pos] == '\'')
+	{
+		nt->value = ft_substr(rt, start, mshll->pos - start + 1);
+		nt->type = t;
+		return ;
+	}
+	else
+	{
+		printf("single quote do not close\n");
+		exit (0);
+	}
+}
+
+void	d_quote_case(t_input *mshll, t_token *nt, char *rt, t_tokentype t)
+{
+	int	start;
+
+	start = mshll->pos;
+	mshll->pos++;
+	while (mshll->line[mshll->pos] != '\"' && mshll->line[mshll->pos])
+		mshll->pos++;
+	if (mshll->line[mshll->pos] == '\"')
+	{
+		nt->value = ft_substr(rt, start, mshll->pos - start + 1);
+		nt->type = t;
+		return ;
+	}
+	else
+	{
+		printf("double quote do not close\n");
+		exit (0);
+	}
+}
