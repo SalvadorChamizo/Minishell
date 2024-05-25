d8d151ece057d110ce865efafa019267d2624c82
Sergio Aroca <saroca1993@gmail.com>
2024-05-25 10:54:38 +0200

    Signal 2/2
    Now, the code manage the signal that subjects indicated when the terminal is interactive, but now the next challengue is manage this signal when the terminal is not interactibe.
    I include a five code lines in input_init function that manage contr-d signal like bash when is interactive.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: saroca-f <saroca-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 17:56:12 by saroca-f          #+#    #+#             */
-/*   Updated: 2024/05/24 19:50:55 by saroca-f         ###   ########.fr       */
+/*   Updated: 2024/05/25 10:47:19 by saroca-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,23 @@
 
 volatile sig_atomic_t sigint_received = 0;
 
-void signal_handler(int signal_number) 
+void signal_c(int signal_number) 
 {
 	if (signal_number == SIGINT)
 	{
-		printf("\n");
-        rl_replace_line("", 0);
-		rl_on_new_line();
-        rl_redisplay();
-		sigint_received = 1;
-    }
+		if (isatty(STDIN_FILENO))
+		{
+			printf("\n");
+			rl_replace_line("", 0);
+			rl_on_new_line();
+			rl_redisplay();
+			sigint_received = 1;
+		}
+		else
+		{
+			sigint_received = 1;
+		}
+	}
 }
 
 //////////////////////////////////////////
@@ -37,6 +44,11 @@ void	input_init(t_input *input, char **ev)
 	i = 0;
 	input->pos = 0;
 	input->line = readline(RED"minishell> "RESET);
+	if (input->line == NULL && isatty(STDIN_FILENO)) // Detectar Ctrl+D cuando es interactivo
+	{
+		printf("exit\n");
+		exit(0);
+	}
 	while (ev[i] != NULL)
 	{
 		if (ft_strcmp(ev[i], "PATH="))
@@ -56,7 +68,7 @@ int	main(int argc, char **argv, char **env)
 	(void)argc;
 	(void)argv;
 	ft_enter(); //eslogan de entrada
-	signal(SIGINT, signal_handler);
+	signal(SIGINT, signal_c);
 	signal(SIGQUIT, SIG_IGN);
 	while (1)
 	{
@@ -68,11 +80,11 @@ int	main(int argc, char **argv, char **env)
 			sigint_received = 0;
 			free(input->line);
         }*/
-		if (!ft_strncmp(input->line, "exit", 4))
-		{
-			ft_exit(); //eslogan de salida
-			break ;
-		}
+		 if (!strncmp(input->line, "exit", 4)) 
+        {
+            ft_exit(); // eslogan de salida
+            break;
+        }
 		else
 		{
 			input->pos = 0;
