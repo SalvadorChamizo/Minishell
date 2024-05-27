9af64ed2e96eafe8ddf78a7c353b8af50dd9bbaa
Salvador Chamizo <salvadorchamizo@gmail.com>
2024-05-27 15:51:40 +0200

    Added changes to manage env. variables
    Now there are some new functions to haddle env. variables in different
    ways. For example, multiple env. variables in a single argument, env.
    variables without assignments and env. variables with assignments.

--- expanser/expanser.c
+++ expanser/expanser.c
@@ -6,7 +6,7 @@
 /*   By: schamizo <schamizo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 15:11:31 by schamizo          #+#    #+#             */
-/*   Updated: 2024/05/24 15:31:21 by schamizo         ###   ########.fr       */
+/*   Updated: 2024/05/27 14:57:07 by schamizo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -141,16 +141,32 @@ void	expand_assignment(t_ast *ast, t_ast *prev)
 	expand_assignment(ast->right, ast);
 }
 
-t_assign_list	*new_assignment(char *text)
+t_assign_list	*new_assignment(char *text, t_assign_list *list)
 {
 	t_assign_list	*new_node;
+	t_assign_list	*temp;
+	char			*word;
 	int				i;
 
 	i = 0;
-	new_node = malloc(sizeof(t_assign_list));
 	while (text[i] != '=')
 		i++;
-	new_node->variable = ft_substr(text, 0, i);
+	word = ft_substr(text, 0, i);
+	if (list)
+	{
+		temp = list;
+		while (temp)
+		{
+			if (!ft_strcmp(word, temp->variable))
+			{
+				temp->value = ft_substr(text, i + 1, ft_strlen(text) - i);
+				return (NULL);
+			}
+			temp = temp->next;
+		}
+	}
+	new_node = malloc(sizeof(t_assign_list));
+	new_node->variable = word;
 	new_node->value = ft_substr(text, i + 1, ft_strlen(text) - i);
 	new_node->next = NULL;
 	return (new_node);
@@ -181,14 +197,224 @@ void	store_assignment(t_ast *ast, t_assign_list **list)
 		return ;
 	if (ast->type == N_ASSIGN)
 	{
-		new_node = new_assignment(ast->token->value);
+		new_node = new_assignment(ast->token->value, *list);
 		ft_assign_add_back(list, new_node);
 	}
 	store_assignment(ast->left, list);
 	store_assignment(ast->right, list);
 }
+int	ft_check_dollar(char *text)
+{
+	int	i;
 
-void	ft_expanser(t_ast *ast)
+	i = 0;
+	if (!text)
+		return (0);
+	while (text[i])
+	{
+		if (text[i] == '$' && text[i + 1] != ' '
+			&& text[i + 1] != '\0' && text[i + 1] != '\"')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+/*void ft_dollar(t_ast *ast, t_assign_list *list)
+{
+	char			*text;
+	t_assign_list	*temp;
+	int				flag;
+	int				len;
+
+	flag = 0;
+	len = 0;
+	if (ast == NULL)
+		return ;
+	if (ft_check_dollar(ast->token->value) && ast->token->value[0] != '\'')
+	{
+		if (!list)
+			ast->token->value = ft_substr(ast->token->value, );
+		else if (list)
+		{
+			temp = list;
+			if (ast->token->type != N_COMMAND)
+				text = ft_substr(ast->token->value, 1, ft_strlen(ast->token->value) - 1);
+			else
+				text = ft_substr(ast->token->value, 2, ft_strlen(ast->token->value) - 2);
+			while (temp)
+			{
+				if (ft_strcmp(text, temp->variable) == 0)
+				{
+					ast->token->value = temp->value;
+					flag = 1;
+				}
+				temp = temp->next;
+			}
+			if (flag == 0)
+				ast->token->value = "";
+		}
+	}
+	ft_dollar(ast->left, list);
+	ft_dollar(ast->right, list);
+}*/
+
+char	*remove_dollar(t_ast *ast)
+{
+	char	*text;
+	char	*new_text;
+	int		i;
+	int		j;
+
+	text = ft_strdup(ast->token->value);
+	new_text = malloc(sizeof(char) * ft_strlen(text));
+	i = 0;
+	j = 0;
+	while (text[i] != '\0')
+	{
+		if (text[i] == '$')
+		{
+			while (text[i] != ' ' && text[i] != '\0')
+				i++;
+		}
+		new_text[j] = text[i];
+		if (text[i] != '\0')
+			i++;
+		j++;
+	}
+	new_text[j] = '\0';
+	return (new_text);
+}
+
+char	*get_variable(char	*text, int *cur)
+{
+	char	*variable;
+	int		i;
+	int		len;
+
+	variable = NULL;
+	len = 0;
+	i = *cur + 1;
+	if (text[*cur] == '$')
+	{
+		while(text[*cur] != ' ' && text[*cur] != '\0')
+		{
+			*cur = *cur + 1;
+			len++;
+		}
+		printf("Falla en el malloc\n");
+		variable = malloc(sizeof(char) * len);
+		printf("O no\n");
+		len = 0;
+		while (text[i] != ' ' && text[i] != '\0' && text[i] != '\"')
+		{
+			printf("Falla en el bucle i = %d\n", i);
+			variable[len] = text[i];
+			len++;
+			i++;
+		}
+	}
+	return (variable);
+}
+
+void	ft_dollar_list(t_ast *ast, t_assign_list *list, int *flag)
+{
+	t_assign_list	*temp;
+	char			*text;
+	char			*variable;
+	int				i;
+	int				j;
+	int				k;
+	char			*new_text;
+
+
+	temp = list;
+	text = ast->token->value;
+	i = 0;
+	j = 0;
+	k = 0;
+	new_text = malloc(sizeof(char) * ft_strlen(text) * 2);
+	while (text[i])
+	{
+		printf("Falla en i = %d\n", i);
+		if (text[i] == '$')
+		{
+			printf("Antes de get_variable\n");
+			variable = get_variable(text, &i);
+			printf("Después de get_variable\n");
+			printf("Variable es: %s\n", variable);
+			while (temp)
+			{
+				if (ft_strcmp(variable, temp->variable) == 0)
+				{
+					while (temp->value[j])
+					{
+						printf("Llega a entrar aquí\n");
+						new_text[k] = temp->value[j];
+						j++;
+						k++;
+					}
+					*flag = 1;
+					j = 0;
+					break ;
+				}
+				temp = temp->next;
+			}
+			temp = list;
+		}
+		new_text[k] = text[i];
+		k++;
+		i++;
+	}
+	ast->token->value = new_text;
+}
+
+void ft_dollar(t_ast *ast, t_assign_list *list)
+{
+	int				flag;
+	int				len;
+
+	flag = 0;
+	len = 0;
+	if (ast == NULL)
+		return ;
+	if (ft_check_dollar(ast->token->value) && ast->token->value[0] != '\'')
+	{
+		printf("Estamos dentro\n");
+		if (!list)
+			ast->token->value = remove_dollar(ast);
+		else if (list)
+		{
+			ft_dollar_list(ast, list, &flag);
+			printf("Valor de flag = %d\n", flag);
+			if (flag == 0)
+				ast->token->value = remove_dollar(ast);
+		}
+	}
+	ft_dollar(ast->left, list);
+	ft_dollar(ast->right, list);
+}
+
+void	expand_quotes(t_ast *ast)
+{
+	if (ast == NULL)
+		return ;
+	while (ast->token->value[0] == '\"' || ast->token->value[0] == '\'')
+	{
+		if (ast->token->value[0] == '\"')
+		{
+			ast->token->value = ft_strtrim(ast->token->value, "\"");
+		}
+		if (ast->token->value[0] == '\'')
+		{
+			ast->token->value = ft_strtrim(ast->token->value, "\'");
+		}
+	}
+	expand_quotes(ast->left);
+	expand_quotes(ast->right);
+}
+
+void	ft_expanser(t_ast *ast, t_minishell *minishell)
 {
 	t_ast			*ast_temp;
 	t_assign_list	*list;
@@ -196,11 +422,14 @@ void	ft_expanser(t_ast *ast)
 	list = NULL;
 	ast_temp = ast;
 	expand_redir(ast, NULL, 0);
+	printf("Entramos\n");
+	ft_dollar(ast, minishell->list);
+	expand_quotes(ast);
 	expand_assignment(ast, NULL);
 	expand_command(ast, NULL, 0);
 	expand_builtin(ast);
 	expand_command2(ast);
 	//print_ast(ast);
-	store_assignment(ast, &list);
-	print_assignment(list);
+	store_assignment(ast, &minishell->list);
+	print_assignment(minishell->list);
 }
