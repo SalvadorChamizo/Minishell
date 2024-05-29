7fefa3dd899db3efec82dfacb982f1b36f30cf87
Salvador Chamizo <salvadorchamizo@gmail.com>
2024-05-29 18:24:35 +0200

    Added staff for future updates

--- executer/executer.c
+++ executer/executer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   executer.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: saroca-f <saroca-f@student.42.fr>          +#+  +:+       +#+        */
+/*   By: schamizo <schamizo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 18:47:22 by schamizo          #+#    #+#             */
-/*   Updated: 2024/05/29 11:13:30 by saroca-f         ###   ########.fr       */
+/*   Updated: 2024/05/29 15:12:12 by schamizo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,53 @@ void	execute_builtin(t_ast *ast, char **env)
         ft_echo(ast);
     else if (!ft_strcmp(ast->token->value, "env"))
         ft_env(env);
-    else if (!ft_strcmp(ast->token->value, "export"))
-        ft_export(ast, env);
-    /*else if (!ft_strcmp(ast->token->value, "unset"))
+    /*else if (!ft_strcmp(ast->token->value, "export"))
+        ft_export(ast->right);
+    else if (!ft_strcmp(ast->token->value, "unset"))
         ft_unset(ast->right);
     else if (!ft_strcmp(ast->token->value, "exit"))
         ft_exit(ast->right);*/
 }
+
+/*void    ft_command(t_ast *ast)
+{
+    if (ast->right)
+    {
+        ft_pipeline(ast->right);
+    }
+}*/
+
+void    ft_pipeline(t_ast *ast, char **env)
+{
+    if (ast->type == N_PIPELINE)
+    {
+        ft_pipeline(ast->left, env);
+        ft_pipeline(ast->right, env);
+    }
+    if (ast->type == N_HEREDOC || ast->type == N_REDIRECTION)
+    {
+
+    }
+    if (ast->type == N_COMMAND)
+    {
+        //ft_command(ast);
+    }
+    if (ast->type == N_BUILTIN)
+    {
+        execute_builtin(ast, env);
+    }
+}
+
+
 void    ft_executer(t_ast *ast, char **env)
 {
-    execute_builtin(ast, env);
+    if (ast->type == N_PIPELINE)
+    {
+        ft_pipeline(ast->left, env);
+        ft_pipeline(ast->right, env);
+    }
+    if (ast->type == N_BUILTIN)
+        execute_builtin(ast, env);
+    //if (ast->type == N_COMMAND)
+        //ft_command(ast);
 }
