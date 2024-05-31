13e67ffe591ef5475fe5aafe4d8795c72fbf71a2
Salvador Chamizo <salvadorchamizo@gmail.com>
2024-05-31 15:19:26 +0200

    Leaks solved when Ctrl+D is used
    The assignment list wasn't been freed in Ctrl+D exit case.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: schamizo <schamizo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 17:56:12 by saroca-f          #+#    #+#             */
-/*   Updated: 2024/05/31 14:53:22 by schamizo         ###   ########.fr       */
+/*   Updated: 2024/05/31 15:18:53 by schamizo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,7 @@ int	main(int argc, char **argv, char **env)
 		if (input_init(minishell->input) == 1)
 		{
 			ft_exit();
+			ft_list_clear(&minishell->list);
 			free(minishell);
 			exit(0);
 		}
