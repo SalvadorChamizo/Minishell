90cd73228d2dbd39e1e2bbc994ef42098a6088d1
Salvador Chamizo <salvadorchamizo@gmail.com>
2024-06-04 19:00:06 +0200

    Norminette errors, heredoc and pipes
    Now the pipes and the heredoc work properly. Some problems have been
    solved, related to this.
    
    Also the norminette it's almost perfect. Just a few things are left to
    be fixed.

--- executer/executer.c
+++ executer/executer.c
@@ -6,113 +6,26 @@
 /*   By: schamizo <schamizo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 18:47:22 by schamizo          #+#    #+#             */
-/*   Updated: 2024/05/31 18:51:29 by schamizo         ###   ########.fr       */
+/*   Updated: 2024/06/04 17:03:24 by schamizo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../bash.h"
 
-void	execute_builtin(t_ast *ast, char ***env, t_assign_list **list)
+void	execute_builtin(t_ast *ast, t_minishell *minishell)
 {
 	if (!ft_strcmp(ast->token->value, "cd"))
-		ft_cd(ast, *env);
+		ft_cd(ast, minishell->env);
 	else if (!ft_strcmp(ast->token->value, "pwd"))
 		ft_pwd();
 	else if (!ft_strcmp(ast->token->value, "echo"))
 		ft_echo(ast);
 	else if (!ft_strcmp(ast->token->value, "env"))
-		ft_env(*env);
+		ft_env(minishell->env);
 	else if (!ft_strcmp(ast->token->value, "export"))
-		ft_export(ast, env, *list);
+		ft_export(ast, &minishell->env, minishell->list);
 	else if (!ft_strcmp(ast->token->value, "unset"))
-		ft_unset(ast, list);
-}
-
-char	**ft_command_args(t_ast *ast)
-{
-	t_ast	*temp;
-	char	**args;
-	int		i;
-
-	temp = ast;
-	i = 0;
-	while (temp)
-	{
-		temp = temp->left;
-		i++;
-	}
-	temp = ast;
-	args = malloc(sizeof(char *) * i);
-	i = 0;
-	while (temp)
-	{
-		if (temp->token->value != NULL)
-			args[i] = temp->token->value;
-		temp = temp->left;
-		i++;
-	}
-	return (args);
-}
-
-void	ft_simple_command(t_ast *ast, char ***env, t_assign_list **list)
-{
-	pid_t	pid;
-
-	if (ast->right)
-		ft_executer(ast->right, env, list);
-	if (access(ast->token->value, F_OK | X_OK) != 0)
-	{
-		printf("Command \"%s\" not found\n", ast->token->value);
-		return ;
-	}
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("fork");
-		exit(1);
-	}
-	else if (pid == 0)
-	{
-		execve(ast->token->value, ft_command_args(ast), *env);
-	}
-	else
-	{
-		waitpid(pid, NULL, 0);
-		//printf("\n");
-	}
-}
-
-void	ft_command(t_ast *ast, char ***env, t_assign_list **list)
-{
-	pid_t	pid;
-	char	**args;
-	t_ast	*temp;
-	int		cont;
-	int		i;
-
-	cont = 0;
-	i = 0;
-	temp = ast;
-	if (ast->right)
-	{
-		ft_executer(ast->right, env, list);
-	}
-	if (access(ast->token->value, F_OK | X_OK) != 0)
-	{
-		printf("Command \"%s\" not found\n", ast->token->value);
-		return ;
-	}
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("fork");
-		exit(1);
-	}
-	else if (pid == 0)
-	{
-		args = ft_command_args(ast);
-		execve(ast->token->value, args, *env);
-	}
+		ft_unset(ast, &minishell->list);
 }
 
 int	check_files(t_ast *ast)
@@ -130,124 +43,30 @@ int	check_files(t_ast *ast)
 	return (0);
 }
 
-void	ft_open_infile(t_ast *ast)
+void	manage_error(char *error)
 {
-	int	fd;
-
-	if (!check_files(ast))
-	{
-		fd = open(ast->token->value, O_RDONLY);
-		if (fd < 0)
-		{
-			perror("open");
-		}
-		dup2(fd, STDIN_FILENO);
-	}
+	perror(error);
+	exit(EXIT_FAILURE);
 }
 
-void	ft_open_outfile(t_ast *ast)
-{
-	int	fd;
-
-	if (access(ast->token->value, F_OK) != 0)
-	{
-		fd = open(ast->token->value, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		if (fd < 0)
-		{
-			perror("open");
-		}
-		dup2(fd, STDOUT_FILENO);
-	}
-	else
-	{
-		fd = open(ast->token->value, O_WRONLY | O_TRUNC, 0644);
-		if (fd < 0)
-		{
-			perror("open");
-		}
-		dup2(fd, STDOUT_FILENO);
-	}
-}
-
-void	ft_open_outfile_2(t_ast *ast)
-{
-	int	fd;
-
-	if (access(ast->token->value, F_OK) != 0)
-	{
-		fd = open(ast->token->value, O_WRONLY | O_CREAT | O_APPEND, 0644);
-		if (fd < 0)
-		{
-			perror("open");
-		}
-		dup2(fd, STDOUT_FILENO);
-	}
-	else
-	{
-		fd = open(ast->token->value, O_WRONLY | O_APPEND, 0644);
-		if (fd < 0)
-		{
-			perror("open");
-		}
-		dup2(fd, STDOUT_FILENO);
-	}
-}
-
-void	ft_redirect(t_ast *ast, char ***env, t_assign_list **list)
-{
-	if (ast->right)
-		ft_executer(ast->right, env, list);
-	if (ft_strcmp(ast->token->value, "<") == 0)
-		ft_open_infile(ast->left);
-	else if (ft_strcmp(ast->token->value, ">") == 0)
-		ft_open_outfile(ast->left);
-	else if (ft_strcmp(ast->token->value, ">>") == 0)
-		ft_open_outfile_2(ast->left);
-	/*else if (ast->token->value == '<<')
-		ft_open_heredoc(ast);*/
-}
-
-void	ft_pipeline(t_ast *ast, char ***env, t_assign_list **list)
-{
-	
-	if (ast->type == N_PIPELINE)
-	{
-		ft_pipeline(ast->left, env, list);
-		ft_pipeline(ast->right, env, list);
-	}
-	if (ast->type == N_HEREDOC || ast->type == N_REDIRECTION)
-	{
-
-	}
-	if (ast->type == N_COMMAND)
-	{
-		ft_command(ast, env, list);
-	}
-	if (ast->type == N_BUILTIN)
-	{
-		execute_builtin(ast, env, list);
-	}
-}
-
-void	ft_executer(t_ast *ast, char ***env, t_assign_list **list)
+void	ft_executer(t_ast *ast, t_minishell *minishell)
 {
 	if (!ast)
 		return ;
 	if (ast->type == N_PIPELINE)
 	{
-		ft_pipeline(ast->left, env, list);
-		ft_pipeline(ast->right, env, list);
+		ft_pipeline(ast, minishell);
 	}
 	if (ast->type == N_BUILTIN)
 	{
-		execute_builtin(ast, env, list);
+		execute_builtin(ast, minishell);
 	}
 	if (ast->type == N_COMMAND)
 	{
-		ft_simple_command(ast, env, list);
+		ft_simple_command(ast, minishell);
 	}
-	if (ast->type == N_REDIRECTION)
+	if (ast->type == N_REDIRECTION || ast->type == N_HEREDOC)
 	{
-		ft_redirect(ast, env, list);
+		ft_redirect(ast, minishell);
 	}
 }
