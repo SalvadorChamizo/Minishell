bab94161fa4b88ef62ffd1a99de454af3bde48e7
Salvador Chamizo <salvadorchamizo@gmail.com>
2024-06-05 15:08:14 +0200

    Issue #23 solved. Output when command fails.
    The problem has been solved adding a new function. Now the path is
    removed before printing an error for commands in terminal.

--- executer/executer_command.c
+++ executer/executer_command.c
@@ -6,11 +6,39 @@
 /*   By: schamizo <schamizo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 16:20:55 by schamizo          #+#    #+#             */
-/*   Updated: 2024/06/04 16:49:59 by schamizo         ###   ########.fr       */
+/*   Updated: 2024/06/05 15:02:21 by schamizo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../bash.h"
+
+char	*ft_remove_path(char *command)
+{
+	char	*new_text;
+	int	i;
+	int len;
+	
+	i = 0;
+	if (command[i] != '/')
+		return (command);
+	while (command[i])
+		i++;
+	len = i;
+	while (command[i] != '/')
+		i--;
+	i++;
+	len = len - i;
+	new_text = malloc(sizeof(char) * len);
+	len = 0;
+	while (command[i])
+	{
+		new_text[len] = command[i];
+		len++;
+		i++;
+	}
+	new_text[len] = '\0';
+	return (new_text);
+}
 
 char	**ft_command_args(t_ast *ast)
 {
@@ -30,8 +58,8 @@ char	**ft_command_args(t_ast *ast)
 	i = 0;
 	while (temp)
 	{
-		if (temp->token->value != NULL)
-			args[i] = temp->token->value;
+		if (temp->token->value != NULL && ft_strcmp(temp->token->value, "") != 0)
+			args[i] = ft_remove_path(temp->token->value);
 		temp = temp->left;
 		i++;
 	}
@@ -41,6 +69,8 @@ char	**ft_command_args(t_ast *ast)
 
 void	ft_simple_command2(t_ast *ast, t_minishell *minishell)
 {
+	char	*new_text;
+
 	if (ast->right)
 		ft_executer(ast->right, minishell);
 	if (ast->type == N_BUILTIN)
@@ -52,24 +82,33 @@ void	ft_simple_command2(t_ast *ast, t_minishell *minishell)
 	{
 		if (access(ast->token->value, F_OK | X_OK) != 0)
 		{
-			printf("Command \"%s\" not found\n", ast->token->value);
+			new_text = ft_remove_path(ast->token->value);
+			printf("Command \"%s\" not found\n", new_text);
 			return ;
 		}
-		execve(ast->token->value, ft_command_args(ast), minishell->env);
+		if (execve(ast->token->value, ft_command_args(ast), minishell->env) == -1)
+		{
+			perror("execve");
+			exit(1);
+		}
 	}
 }
+
+
 
 void	ft_simple_command(t_ast *ast, t_minishell *minishell)
 {
 	pid_t	pid;
 	int		original_stdin;
+	char	*new_text;
 
 	original_stdin = dup(STDIN_FILENO);
 	if (ast->right)
 		ft_executer(ast->right, minishell);
 	if (access(ast->token->value, F_OK | X_OK) != 0)
 	{
-		printf("Command \"%s\" not found\n", ast->token->value);
+		new_text = ft_remove_path(ast->token->value);
+		printf("Command \"%s\" not found\n", new_text);
 		return ;
 	}
 	pid = fork();
@@ -80,7 +119,11 @@ void	ft_simple_command(t_ast *ast, t_minishell *minishell)
 	}
 	else if (pid == 0)
 	{
-		execve(ast->token->value, ft_command_args(ast), minishell->env);
+		if (execve(ast->token->value, ft_command_args(ast), minishell->env) == -1)
+		{
+			perror("execve");
+			exit(1);
+		}
 	}
 	else
 	{
