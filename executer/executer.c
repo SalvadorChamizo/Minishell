36708ec50a5031890ed8763a23267383c05a9483
Sergio Aroca <saroca1993@gmail.com>
2024-06-07 12:39:19 +0200

    executer act

--- executer/executer.c
+++ executer/executer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   executer.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: schamizo <schamizo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: saroca-f <saroca-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 18:47:22 by schamizo          #+#    #+#             */
-/*   Updated: 2024/06/06 13:55:54 by saroca-f         ###   ########.fr       */
+/*   Updated: 2024/06/07 12:20:48 by saroca-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ void	execute_builtin(t_ast *ast, t_minishell *minishell)
 	if (ast->right)
 		ft_executer(ast->right, minishell);
 	if (!ft_strcmp(ast->token->value, "cd"))
-		ft_cd(ast, minishell->env);
+		ft_cd(ast, minishell->env, minishell);
 	else if (!ft_strcmp(ast->token->value, "pwd"))
 		ft_pwd();
 	else if (!ft_strcmp(ast->token->value, "echo"))
 		ft_echo(ast);
 	else if (!ft_strcmp(ast->token->value, "env"))
-		ft_env(minishell->env);
+		ft_env(minishell->env, minishell);
 	else if (!ft_strcmp(ast->token->value, "export"))
 		ft_export(ast, &minishell->env, minishell->list);
 	else if (!ft_strcmp(ast->token->value, "unset"))
