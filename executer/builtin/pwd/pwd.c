c4b7001728709cee843f56c7ae3b6f1bfe8dfe6e
Sergio Aroca <saroca1993@gmail.com>
2024-06-10 15:47:03 +0200

    pwd secure

--- executer/builtin/pwd/pwd.c
+++ executer/builtin/pwd/pwd.c
@@ -6,18 +6,37 @@
 /*   By: saroca-f <saroca-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 10:37:46 by saroca-f          #+#    #+#             */
-/*   Updated: 2024/06/07 15:27:37 by saroca-f         ###   ########.fr       */
+/*   Updated: 2024/06/10 15:30:54 by saroca-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../bash.h"
 
-void	ft_pwd(t_minishell *minishell)
+void	ft_pwd(t_minishell *minishell, char **env)
 {
 	char	*pwd;
+	int		i;
 
-	pwd = getcwd(NULL, 0);
-	ft_putendl_fd(pwd, 1);
-	free(pwd);
-	minishell->status = 0;
+	i = 0;
+	pwd = getpwd(NULL, 0);
+	if (pwd)
+	{
+		printf("%s\n", pwd);
+		free(pwd);
+		minishell->status = 0;
+		return ;
+	}
+	while (env[i] && !pwd)
+	{
+		if (ft_strncmp(env[i], "PWD=", 4) == 0)
+		{
+			pwd = ft_substr(env[i], 4, ft_strlen(env[i]) - 4);
+			pritf("%s\n", pwd);
+			free(pwd);
+			minishell->status = 0;
+			return ;
+		}
+		i++;
+	}
+	minishell->status = 1;
 }
