961dafd34259717904efa498b7866204f844f651
Sergio Aroca <saroca1993@gmail.com>
2024-06-21 21:13:46 +0200

    Exit fixed
    Now the funtion accep all kind of arguments and manahe them to make a
    str that join all arguments if the do not have space when them did not
    have.

--- executer/builtin/exit/exit.c
+++ executer/builtin/exit/exit.c
@@ -3,20 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: schamizo <schamizo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: saroca-f <saroca-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 12:44:55 by saroca-f          #+#    #+#             */
-/*   Updated: 2024/06/19 19:32:35 by schamizo         ###   ########.fr       */
+/*   Updated: 2024/06/21 21:12:20 by saroca-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../bash.h"
 
-void	exit_free(t_minishell **minishell, t_ast *ast)
+void	exit_free(t_minishell **minishell, t_ast *ast, char *str)
 {
 	int	i;
 
 	i = 0;
+	if (str)
+		free(str);
 	rl_clear_history();
 	free_ast(&ast);
 	if ((*minishell)->list)
@@ -32,9 +34,30 @@ void	exit_free(t_minishell **minishell, t_ast *ast)
 	free((*minishell));
 }
 
-int	ft_strisnumber(char *str)
+char	*str_maker(t_ast **ast)
 {
-	int	i;
+	char	*str;
+	char	*tep;
+
+	str = NULL;
+	tep = NULL;
+	ft_putstr_fd("exit\n", 1);
+	if (!(*ast))
+		return (str);
+	str = ft_strdup((*ast)->token->value);
+	while ((*ast)->left	&& (*ast)->left->token->space == false)
+	{
+		(*ast) = (*ast)->left;
+		tep = ft_strjoin(str, (*ast)->token->value);
+		free(str);
+		str = tep;
+	}
+	return (str);
+}
+
+int	ft_str_is_number(char *str)
+{
+	int		i;
 
 	i = 0;
 	if (str[i] == '-' || str[i] == '+')
@@ -48,31 +71,40 @@ int	ft_strisnumber(char *str)
 	return (1);
 }
 
+void	ft_exit_text(t_minishell **minishell, t_ast *ast, char *str)
+{
+	ft_putstr_fd("bash: exit: ", 2);
+	ft_putstr_fd(str, 2);
+	ft_putstr_fd(": numeric argument required\n", 2);
+	exit_free(minishell, ast, str);
+	exit (2);
+}
+
 void	ft_exit(t_minishell **minishell, t_ast *ast)
 {
-	int	i;
+	int		i;
+	char	*str;
+	t_ast	*tmp;
 
 	i = 0;
-	ft_putstr_fd("exit\n", 1);
+	tmp = NULL;
 	if (ast->left)
+		tmp = ast->left;
+	str = str_maker(&tmp);
+	if (str)
 	{
-		if (ft_strisnumber(ast->left->token->value))
-			i = ft_atoi(ast->left->token->value);
-		if (!ft_strisnumber(ast->left->token->value))
-		{
-			ft_putstr_fd("bash: exit: ", 2);
-			ft_putstr_fd(ast->left->token->value, 2);
-			ft_putstr_fd(": numeric argument required\n", 2);
-			exit_free(minishell, ast);
-			exit (2);
-		}
-		else if (ast->left->left)
+		if (ft_str_is_number(str))
+			i = atoi(str);
+		else
+			ft_exit_text(minishell, ast, str);
+		if (tmp->left)
 		{
 			ft_putstr_fd("bash: exit: too many arguments\n", 2);
 			(*minishell)->status = 1;
+			free(str);
 			return ;
 		}
 	}
-	exit_free(minishell, ast);
+	exit_free(minishell, ast, str);
 	exit(i);
 }
