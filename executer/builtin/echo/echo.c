66688ad6339bb29317ef653ca84b0c90614d949d
Sergio Aroca <saroca1993@gmail.com>
2024-06-21 22:09:38 +0200

    echo flag with quotes fixed

--- executer/builtin/echo/echo.c
+++ executer/builtin/echo/echo.c
@@ -3,33 +3,64 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: schamizo <schamizo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: saroca-f <saroca-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 12:04:59 by saroca-f          #+#    #+#             */
-/*   Updated: 2024/06/21 19:27:00 by schamizo         ###   ########.fr       */
+/*   Updated: 2024/06/21 22:07:06 by saroca-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../bash.h"
 
-bool n_flag(char *str)
+char	*str_echo_maker(t_ast **ast)
 {
-	int i;
+	char	*str;
+	char	*tep;
+
+	str = NULL;
+	tep = NULL;
+	if (!(*ast))
+		return (str);
+	str = ft_strdup((*ast)->token->value);
+	while ((*ast)->left	&& (*ast)->left->token->space == false)
+	{
+		(*ast) = (*ast)->left;
+		tep = ft_strjoin(str, (*ast)->token->value);
+		free(str);
+		str = tep;
+	}
+	return (str);
+}
+
+bool n_flag(t_ast **tmp)
+{
+	int		i;
+	char	*str;
+	t_ast	*ret;
 
 	i = 0;
+	ret = *tmp;
+	str = str_echo_maker(&ret);
 	if (str[i] != '-')
+	{
+		free(str);
 		return (false);
+	}
 	i++;
 	while (str[i] == 'n')
 		i++;
 	if (str[i] != '\0' && !ft_isspace(str[i]))
+	{
+		free(str);
 		return (false);
+	}
+	*tmp = ret;
+	free(str);
 	return (true);
 }
 
 void	echo_print(t_ast *tmp)
 {
-	//ft_putstr_fd(tmp->token->value, 1);
 	if (ft_strcmp(tmp->token->value, "") != 0)
 		printf("%s", tmp->token->value);
 	else
@@ -50,7 +81,7 @@ void	ft_echo(t_ast *ast, t_minishell *minishell)
 		return ;
 	}
 	flag = 0;
-	while (tmp && n_flag(tmp->token->value))
+	while (tmp && n_flag(&tmp))
 	{
 		flag = 1;
 		tmp = tmp->left;
