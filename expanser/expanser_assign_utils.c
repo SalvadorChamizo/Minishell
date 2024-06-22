d2ab4246c5ba4dc3cda88889f141e1e54852f966
Sergio Aroca <saroca1993@gmail.com>
2024-06-22 10:26:12 +0200

    I forgot to commit new files to assigment

---
+++ expanser/expanser_assign_utils.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expanser_assign_utils.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saroca-f <saroca-f@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/22 10:01:01 by saroca-f          #+#    #+#             */
+/*   Updated: 2024/06/22 10:12:56 by saroca-f         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "../bash.h"
+
+void	quote_manage(char c, int *flag, int *i)
+{
+	if (c == '\'' && *flag != 2)
+	{
+		if (*flag == 0)
+			*flag = 1;
+		else
+			*flag = 0;
+		(*i)++;
+	}
+	if (c == '\"' && *flag != 1)
+	{
+		if (*flag == 0)
+			*flag = 2;
+		else
+			*flag = 0;
+		(*i)++;
+	}
+}
+
+int	new_value_size(char *value)
+{
+	int	i;
+	int	j;
+	int	flag;
+
+	i = 0;
+	j = 0;
+	flag = 0;
+	while (value[i])
+	{
+		quote_manage(value[i], &flag, &i);
+		i++;
+		j++;
+	}
+	return (j);
+}
